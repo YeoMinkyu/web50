ea6c830c9ab17ce4cb7837759b19d2da30a70035
Minkyu Yeo <mk@Minkyuui-MacBookPro.local>
2024-08-09 01:18:40 +0900

    The user can read the email on a view
    - Add 'div' for reading an email in inbox.html.
    - A user can see the contents of the email.
     (sender, recipients, subject, timestamp and body)
    - Once the email has been read, it's been marked as read(true status).

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -17,6 +17,7 @@ function compose_email() {
   // alert("Debug : compose_email");
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -29,6 +30,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#read-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
@@ -47,7 +49,8 @@ function load_mailbox(mailbox) {
 
       emails.forEach(email => {
         let inbox_row = document.createElement('div');
-        inbox_row.id = 'inbox-row';
+        inbox_row.classList.add('inbox-row');
+        inbox_row.id = email.id;
 
         let sender_element = document.createElement('div');
         let subject_element = document.createElement('div');
@@ -69,12 +72,12 @@ function load_mailbox(mailbox) {
           inbox_row.style.backgroundColor = 'white';
         }
 
-        inbox_container.append(inbox_row);
-      }); 
+        inbox_row.addEventListener('click', function() {
+          view_email(this.id);
+        });
 
-      // element.addEventListener('click', function() {
-      //     console.log('This element has been clicked!')
-      // });
+        inbox_container.append(inbox_row);
+      });
 
   })
 }
@@ -107,4 +110,56 @@ function send_email() {
   });
 
   return false;
+}
+
+function view_email(email_id) {
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#read-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+
+  // alert(email.subject);
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+
+    // ... do something else with email ...
+    document.querySelector('#read-view').innerHTML = `
+    <div id='email-info'></div>
+    <hr>
+    <div id='email-body'></div>
+    `;
+    let email_info = document.querySelector('#email-info');
+    let email_body = document.querySelector('#email-body');
+
+    let email_sender = document.createElement('p');
+    let email_recipients = document.createElement('p');
+    let email_subject = document.createElement('p');
+    let email_timestamp = document.createElement('p');
+    let email_contents = document.createElement('p');
+    let reply_button = document.createElement('button');
+
+    email_sender.innerHTML = `<b>From: </b>${email.sender}`;
+    email_recipients.innerHTML = `<b>To: </b>${email.recipients}`;
+    email_subject.innerHTML = `<b>Subject: </b>${email.subject}`;
+    email_timestamp.innerHTML = `<b>Timestamp: </b>${email.timestamp}`;
+    reply_button.innerHTML = 'Reply';
+    reply_button.type = 'button';
+    reply_button.classList.add('btn', 'btn-outline-primary');
+
+    email_contents.innerHTML = email.body;
+
+    email_info.append(email_sender, email_recipients, email_subject, email_timestamp, reply_button);
+    email_body.append(email_contents);
+  });
+
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
 }
